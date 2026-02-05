77fa128e681f9b0bacdcdb95227d7d454be64cd3
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-02-05 13:18:55 +0100

    first commit

---
+++ mail/main.js
@@ -0,0 +1,27 @@
+//TRACCIA
+
+
+
+/*
+
+
+Mail
+Crea una lista di email di invitati ad una festa.
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
+Si può fare? Certo che si basta ragionare un po’.
+Nota: 
+Non è necessario provvedere alla validazione delle email
+
+
+Consigli del giorno:
+scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+si ma noi cosa vogliamo fare?
+torniamo a scrivere in italiano
+proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+
+
+*/
