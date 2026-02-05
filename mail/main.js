40651210239a4dee6e5aeca3a9e076fe4d04d04d
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-02-05 13:56:33 +0100

    Add human code for mail

--- mail/main.js
+++ mail/main.js
@@ -10,8 +10,10 @@ Crea una lista di email di invitati ad una festa.
 Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+
 Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
 Si può fare? Certo che si basta ragionare un po’.
+
 Nota: 
 Non è necessario provvedere alla validazione delle email
 
@@ -25,3 +27,37 @@ proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro program
 
 
 */
+
+
+
+//HUMAN CODE
+
+//COSA DEVE FARE IL SOFTWARE:
+
+
+/*
+-creare una whitelist di email
+-domandare all'utente la sua email
+    SE l'email è nella whitelist
+        -notifico l'utente può accedere
+    ALTRIMENTI
+        -notifico l'utente che non può accedere
+
+*/
+
+//SEMI-CODE
+
+/*
+
+-creo un array di stringhe
+-creo un prompt
+    IF il prompt == array
+        -loggo/let messaggio che può accedere
+
+    ELSE 
+        --loggo/let messaggio che non può accedere
+
+
+*/
+
+
